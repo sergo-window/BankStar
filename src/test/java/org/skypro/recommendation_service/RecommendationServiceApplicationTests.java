18c28136e4c3e69a3a5a8d17479b68f116f11c34
Сергей <sergowindow@yandex.ru>
2025-11-05 17:45:17 +0300

    initial commit

---
+++ src/test/java/org/skypro/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
